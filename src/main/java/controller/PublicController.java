a657578c57b8f5a0d93ce68af1f191ef666ffea7
Binary <49093421+shuhaohwang@users.noreply.github.com>
2020-02-18 09:51:43 +0800

    项目对表单获取对象部分进行优化

--- src/main/java/controller/PublicController.java
+++ src/main/java/controller/PublicController.java
@@ -1,12 +1,9 @@
 package controller;
 
 import model.User;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import service.UserService;
 
@@ -23,18 +20,19 @@ public class PublicController {
     @Autowired
     private UserService service;
 
-    @RequestMapping("/register")
+
     @ResponseBody
-    public Object User_Register(String username, String email, String password) {
-        System.out.println("执行注册");
-        System.out.println("前端传来的数据：" + username + " " + email + " " + password);
-
-        User user = new User();
-
-        user.setUsername(username);
-        user.setEmail(email);
-        user.setPassword(password);
-
+    @RequestMapping("/register")
+    public Object User_Register(User user) {
+//        System.out.println("执行注册");
+//        System.out.println("前端传来的数据：" + username + " " + email + " " + password);
+//
+//        User user = new User();
+//
+//        user.setUsername(username);
+//        user.setEmail(email);
+//        user.setPassword(password);
+        System.out.println(user);
 
         return service.register(user);
     }
